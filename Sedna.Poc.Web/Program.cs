cb31416b8cd8156d105771e07d929ed84f904777
NFK718 <nellishobbes@gmail.com>
2024-03-24 18:58:09 -0400

    Fix cors API connection issue

--- Sedna.Poc.Web/Program.cs
+++ Sedna.Poc.Web/Program.cs
@@ -75,6 +75,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
