9f4be9068edadfcda1f0d238cbd03087ab9ca6e9
NFK718 <nellishobbes@gmail.com>
2024-03-24 18:56:18 -0400

    Add check-user API endpoint & JWT generator

--- Sedna.Poc.Web/Controllers/UserController.cs
+++ Sedna.Poc.Web/Controllers/UserController.cs
@@ -3,10 +3,17 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Sedna.Poc.Data.Entities;
 using Sedna.Poc.Lib.Services;
+using Sedna.Poc.Data.Models;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System;
+using dotenv.net;
 
 namespace Sedna.Poc.Web.Controllers;
 
-[ApiController, Authorize, Route("api/v1/users")]
+[ApiController, Route("api/v1/users")]
 public class UserController : ControllerBase
 {
     private readonly UserService _userService;
@@ -45,4 +52,42 @@ public class UserController : ControllerBase
             return BadRequest(e.Message);
         }
     }
+
+    [HttpPost("check-user")]
+    public async Task<IActionResult> CheckUser([FromBody] UserCheckModel model)
+    {
+        var (exists, statusCode, user) = await _userService.CheckUserExistsAsync(model.Email, model.Pass);
+    
+        if (!exists)
+        {
+            return NotFound();
+        }
+
+        var token = GenerateJwtToken(model);
+
+        return Ok(new {
+        jwtToken = token, 
+        payload = new { 
+            email = user.Email,
+            given_name = user.FirstName,
+            family_name = user.LastName
+        }
+        });
+    }
+
+    private string GenerateJwtToken(UserCheckModel model)
+    {
+        DotEnv.Load();
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes("placeholder_value_for_jwt_secret_environment_variable");
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[] { new Claim("email", model.Email.ToString()) }),
+            Expires = DateTime.UtcNow.AddDays(1),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+
 }
